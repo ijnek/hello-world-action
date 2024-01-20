d02a7e81bdb9f3606f09bdd890487117b05eec3a
ijnek <kenjibrameld@gmail.com>
2024-01-20 03:14:42 +0000

    bla

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const octokit = github.getOctokit(process.env.GITHUB_TOKEN || '');
 
-try {
-  await octokit.rest.git.createTree({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    tree: jazzy,
-  });
+octokit.rest.git.createTree({
+  owner: github.context.repo.owner,
+  repo: github.context.repo.repo,
+  tree: jazzy,
+});
 
-} catch (err) {
-  core.warning(`Failed to automatically add repo tag, manually tag with:   'git tag ${tag} ${github.context.sha}'`);
-  core.debug(`${err}`);
-  return false;
-}
